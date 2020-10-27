d17bd78d82641d8bcf72e29cfa79099340e17fb7
Dan Webb <dan.webb@damacus.io>
2020-10-27 14:54:02 +0000

    DEBUG

--- tests/controls/python_spec.rb
+++ tests/controls/python_spec.rb
@@ -13,29 +13,29 @@ control 'Python version' do
   end
 end
 
-control 'Pandas can read a CSV' do
-  impact 'high'
-  title 'Python Pandas is installed and can read a CSV'
-  desc 'Reading from a CSV is a common task'
-  tag 'python'
-  only_if { ::File.exist?('/tests/pandas_read_csv.py') }
+# control 'Pandas can read a CSV' do
+#   impact 'high'
+#   title 'Python Pandas is installed and can read a CSV'
+#   desc 'Reading from a CSV is a common task'
+#   tag 'python'
+#   only_if { ::File.exist?('/tests/pandas_read_csv.py') }
 
-  describe command('python /tests/pandas_read_csv.py') do
-    its('stdout') { should match /foo bar baz/ }
-    its('exit_status') { should eq 0 }
-  end
-end
+#   describe command('python /tests/pandas_read_csv.py') do
+#     its('stdout') { should match /foo bar baz/ }
+#     its('exit_status') { should eq 0 }
+#   end
+# end
 
-control 'Pandas can read a CSV from S3' do
-  impact 'high'
-  title 'Python Pandas is installed and can read a CSV from s3'
-  desc 'Python Pandas is installed and can read a CSV from s3'
-  tag 'python'
-  tag 'known_broken'
-  only_if { ::File.exist?('/tests/pandas_read_s3.py')}
+# control 'Pandas can read a CSV from S3' do
+#   impact 'high'
+#   title 'Python Pandas is installed and can read a CSV from s3'
+#   desc 'Python Pandas is installed and can read a CSV from s3'
+#   tag 'python'
+#   tag 'known_broken'
+#   only_if { ::File.exist?('/tests/pandas_read_s3.py')}
 
-  describe command('python /tests/pandas_read_s3.py') do
-    its('stdout') { should_not match /foo bar baz/ }
-    its('exit_status') { should_not eq 0 }
-  end
-end
+#   describe command('python /tests/pandas_read_s3.py') do
+#     its('stdout') { should_not match /foo bar baz/ }
+#     its('exit_status') { should_not eq 0 }
+#   end
+# end
