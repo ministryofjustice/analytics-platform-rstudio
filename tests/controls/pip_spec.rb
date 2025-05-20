6fe496ab6c43749bc8729d63085375f28a3de5ca
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-05-20 15:38:18 +0100

    Attempt to fix test by using venv

--- tests/controls/pip_spec.rb
+++ tests/controls/pip_spec.rb
@@ -25,7 +25,12 @@ control "osmnx" do
   tag "installer"
   tag "pip"
 
-  describe command("pip3 install -vvv osmnx") do
+  describe bash("
+    python3 -m venv /tmp/osmnx_venv &&
+    source /tmp/osmnx_venv/bin/activate &&
+    pip install --upgrade pip &&
+    pip install osmnx
+  ") do
     its("exit_status") { should eq 0 }
     its("stdout") { should match /Successfully installed/ }
   end
