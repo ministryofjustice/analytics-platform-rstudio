66c9376b83ff3a284d3b730e7e41d3c0c373dfa9
Michael Collins <15347726+michaeljcollinsuk@users.noreply.github.com>
2025-05-21 16:52:28 +0100

    Try removing call to default user script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,5 @@ set -e
 USER_NAME="${USER:-rstudio}"
 export DEFAULT_USER="$USER_NAME"
 
-if id -u 1000 &>/dev/null && [ "$(id -un 1000)" != "$DEFAULT_USER" ]; then
-	echo "Removing default user with UID 1000"
-	userdel --remove "$(id -un 1000)"
-fi
-
-if [ -x /rocker_scripts/default_user.sh ]; then
-	echo "Running default_user.sh"
-	/rocker_scripts/default_user.sh
-else
-	echo "default_user.sh not found or not executable" >&2
-	exit 1
-fi
-
 # Start s6 to run init scripts (including secure-cookie-key-conf)
 /init
